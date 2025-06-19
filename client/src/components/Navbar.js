62ca71cf516711f7613d4c2cc07c2ff674017763
Meenuga UshaRani <usharanimeenuga33@gmail.com>
2025-06-19 20:55:48 +0530

    Update: new logo and UI fixes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
+
+
+
+  
     <div className="bg-pink-600 text-white flex justify-between items-center px-6 py-3 shadow">
       <div className="text-xl font-bold">InstaChat</div>
       <div className="space-x-4">
